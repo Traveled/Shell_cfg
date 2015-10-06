a447777a764788600897465c7ddbaf2b495ebb8e
Traveled <richard.edouard84@gmail.com>
2015-10-06 11:43:37 +0200

    shell customize

---
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,19 @@
+function cupally {C:\ProgramData\Chocolatey\bin\cup.exe all -y -pre}
+new-alias cccp cupally
+new-alias wup Get-WUInstall
+function Get-PCUptime {
+ 
+param($computer)
+
+Trap {write-host "Cannot Connect to $computer" -Foregroundcolor Red;
+	  continue} 
+ 
+$lastboottime = (Get-WmiObject -Class Win32_OperatingSystem -computername $computer -ea "SilentlyContinue").LastBootUpTime
+ 
+$sysuptime = (Get-Date) – [System.Management.ManagementDateTimeconverter]::ToDateTime($lastboottime)
+
+if ($lastboottime -ne $NULL){
+	Write-Host “System($computer) is Uptime since : ” $sysuptime.days “days” $sysuptime.hours `
+	“hours” $sysuptime.minutes “minutes” $sysuptime.seconds “seconds” -Foregroundcolor Gray;
+	}
+}
